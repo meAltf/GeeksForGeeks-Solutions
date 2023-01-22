3e9585a9f55d05e76f54d52618638d856c68b0bb
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-01-23 02:39:39 +0530

    Create null.java

---
+++ LinkedList/School/null.java
@@ -0,0 +1,11 @@
+
+/***************************************************************************************************************
+
+THERE IS NO ANY QUESTION ON GFG, THEIR DIFFICULTY LEVEL IS SCHOOL..
+SO. LET'S GET STARTED WITH DIFFICULTY LEVEL FROM BASIC..
+..
+
+SEE YOU IN NEXT FOLDER OF THIS REPOSITORY
+
+
+****************************************************************************************************************/
