5e25ebaf623f9e84f3ac110a1332675fabd0ac61
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-02-05 23:56:04 +0530

    Create Binary Array Sorting.java

---
+++ Array/Basic/Binary Array Sorting.java
@@ -0,0 +1,56 @@
+/******************************************************************************************************************************************************
+
+Given a binary array A[] of size N. The task is to arrange the array in increasing order.
+Note: The binary array contains only 0  and 1.
+
+---------------------------------------------------------------------------------------
+
+Example 1:
+
+Input: 
+5
+1 0 1 1 0
+
+Output: 
+0 0 1 1 1
+
+Explanation: 
+After arranging the elements in 
+increasing order, elements will be as 
+0 0 1 1 1.
+
+Example 2:
+
+Input:
+10
+1 0 1 1 1 1 1 0 0 0
+
+Output: 
+0 0 0 0 1 1 1 1 1 1
+
+Explanation: 
+After arranging the elements in 
+increasing order, elements will be 
+0 0 0 0 1 1 1 1 1 1.
+
+----------------------------------------------------------------------------------------
+
+*******************************************************************************************************************************************************/
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to sort the binary array.
+    static void binSort(int A[], int N)
+    {
+        // add your code here
+        
+        Arrays.sort(A);
+        
+        
+        /**************
+        * No need to print the array
+        * ************/
+    }
+}
