f00178484e7b1e78f26b05d32510f9fcab1ec683
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-02-17 01:22:38 +0530

    Create Implement Queue using Linked List.java

---
+++ Queue/Basic/Implement Queue using Linked List.java
@@ -0,0 +1,89 @@
+/******************************************************************************************************************************************************
+
+Implement a Queue using Linked List. 
+A Query Q is of 2 Types
+(i) 1 x   (a query of this type means  pushing 'x' into the queue)
+(ii) 2     (a query of this type means to pop an element from the queue and print the poped element)
+
+---------------------------------------------------------------------------------------
+
+Example 1:
+
+Input:
+Q = 5
+Queries = 1 2 1 3 2 1 4 2
+Output: 2 3
+Explanation: n the first testcase
+1 2 the queue will be {2}
+1 3 the queue will be {2 3}
+2   poped element will be 2 the
+    queue will be {3}
+1 4 the queue will be {3 4}
+2   poped element will be 3.
+
+Example 2:
+
+Input:
+Q = 4
+Queries = 1 2 2 2 1 3 
+Output: 2 -1
+Explanation: In the second testcase 
+1 2 the queue will be {2}
+2   poped element will be {2} then
+    the queue will be empty. 
+2   the queue is empty and hence -1
+1 3 the queue will be {3}.
+
+----------------------------------------------------------------------------------------
+
+*******************************************************************************************************************************************************/
+
+
+/*The structure of the node of the queue is
+class QueueNode
+{
+	int data;
+	QueueNode next;
+	QueueNode(int a)
+	{
+	    data = a;
+	    next = null;
+	}
+}*/
+
+class MyQueue
+{
+    QueueNode front, rear;
+    
+    //Function to push an element into the queue.
+	void push(int a)
+	{
+        // Your code here
+       QueueNode n=new QueueNode(a);
+        if(front==null)
+        {
+            front=rear=n;
+        }
+        else
+        {
+            rear.next=n;
+            rear=n;
+      	}
+	}
+	
+    //Function to pop front element from the queue.
+	int pop()
+	{
+        // Your code here
+        if(front==null)
+          {
+            return -1;
+          }
+          else
+          {
+            int data=front.data;
+            front=front.next;
+            return data;
+	      }
+	}
+}
