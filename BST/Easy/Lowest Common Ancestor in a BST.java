907c22d794a93089a40468bf9b4a0be99a31e532
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-03-20 00:43:25 +0530

    Create Lowest Common Ancestor in a BST.java

---
+++ BST/Easy/Lowest Common Ancestor in a BST.java
@@ -0,0 +1,49 @@
+/******************************************************************************************************************************************************
+---------------------
+Level- Easy
+--------------------
+Given a Binary Search Tree (with all values unique) and two node values. Find the Lowest Common Ancestors of the two nodes in the BST.
+
+---------------------------------------------------------------------------------------
+
+Example 1:
+
+Input:
+              5
+           /    \
+         4       6
+          \       \
+           3       7
+                    \
+                     8
+n1 = 7, n2 = 8
+Output: 7
+Example 2:
+
+Input:
+     2
+   /   \
+  1     3
+n1 = 1, n2 = 3
+Output: 2
+
+----------------------------------------------------------------------------------------
+
+*******************************************************************************************************************************************************/
+
+//User function Template for Java
+
+class BST
+{   
+    //Function to find the lowest common ancestor in a BST. 
+	Node LCA(Node root, int n1, int n2)
+	{
+        // code here.   
+        
+        if(root.data < n1 && root.data < n2) return LCA(root.right, n1,n2);
+        
+        if(root.data > n1 && root.data > n2) return LCA(root.left, n1,n2);
+        
+        return root;
+    } 
+}
